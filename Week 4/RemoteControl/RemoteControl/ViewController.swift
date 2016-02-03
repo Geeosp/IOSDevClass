372e9d781d2d0bfede40e4a3fcea15ad371f9536
Geeosp <geeo.sp@gmail.com>
2016-02-02 18:29:03 -0600

    still working

--- Week 4/RemoteControl/RemoteControl/ViewController.swift
+++ Week 4/RemoteControl/RemoteControl/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var power = "ON"
+    var volume = 15
+    var channel = 13
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,54 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    @IBAction func switch_action(sender: UISwitch) {
+        if sender.on{
+            power = "ON"
+        }else{
+            power = "OFF"
+        }
+        update();
+    }
+    
+    @IBAction func slider_action(sender: UISlider) {
+        volume = Int(sender.value)
+        update()
+    }
+    
+    @IBAction func choosedChannel(sender: UISegmentedControl) {
+        switch(sender.selectedSegmentIndex){
+        case 0:
+            channel=13
+            break
+        case 1:
+            channel = 27
+            break;
+        case 2:
+            channel = 50
+            break;
+        case 3:
+            channel = 32
+            
+            break;
+        default:
+            channel = 1;
+            break;
+            
+        }
+        update()
+    }
+    
+    
+    func update(){
+        
+        
+    }
+    func changeChannel(x:Int){
+        
+    }
 
 }
 
