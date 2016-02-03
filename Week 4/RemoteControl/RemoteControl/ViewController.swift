d9a25a91f973ec1caf155300c124de2c7a379d3f
Geeosp <geeo.sp@gmail.com>
2016-02-02 18:55:17 -0600

    Still working

--- Week 4/RemoteControl/RemoteControl/ViewController.swift
+++ Week 4/RemoteControl/RemoteControl/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     var power = "ON"
     var volume = 15
     var channel = 13
-    
+    var dez  = true;
+    @IBOutlet weak var down_view: UIView!
     
     
     
@@ -43,8 +44,10 @@ class ViewController: UIViewController {
     @IBAction func switch_action(sender: UISwitch) {
         if sender.on{
             power = "ON"
+            down_view.userInteractionEnabled=true;
         }else{
             power = "OFF"
+            down_view.userInteractionEnabled=false;
         }
         update();
     }
@@ -83,10 +86,72 @@ class ViewController: UIViewController {
         lb_channel.text = "Channel " + String( channel)
     }
     func changeChannel(x:Int){
+        if(dez){
+            channel = 10*x
+            dez = false;
+        }else{
+            channel += x;
+            dez = true;
+        }
         
         
         update();
     }
+    
+    
+    
+    @IBAction func bt1() {
+        changeChannel(1)
+    }
+   
+    @IBAction func bt2(sender: AnyObject) {
+    changeChannel(2)
+    }
+    
+    @IBAction func bt3(sender: AnyObject) {
+    changeChannel(3)
+    }
+    
+    
+    @IBAction func bt4(sender: AnyObject) {
+    changeChannel(4)
+    }
+    
+    @IBAction func bt5(sender: AnyObject) {
+   changeChannel(5)
+    }
+    
+    
+    @IBAction func bt6(sender: AnyObject) {
+   changeChannel(6)
+    }
+    
+    @IBAction func bt7(sender: AnyObject) {
+        changeChannel(7)
+    }
+    
+    @IBAction func bt8(sender: AnyObject) {
+        changeChannel(8)
+    }
+    
+    @IBAction func bt9(sender: AnyObject) {
+        changeChannel(9)
+    }
+    
+    @IBAction func bt0(sender: AnyObject) {
+        changeChannel(0)
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
