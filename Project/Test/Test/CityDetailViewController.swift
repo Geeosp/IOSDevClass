7c180fd46ae5f1a74c5e38a3a2168985d4e040c4
Geeosp <geeo.sp@gmail.com>
2016-03-09 21:11:39 -0600

    WORKING

--- Project/Test/Test/CityDetailViewController.swift
+++ Project/Test/Test/CityDetailViewController.swift
@@ -7,17 +7,52 @@
 //
 
 import UIKit
-
+import ForecastIOClient
 class CityDetailViewController: UIViewController {
     var cityIndex:Int!
     var city:City!
+    @IBOutlet weak var progressView: UIProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
         city = cities[cityIndex]
         self.title = city.name
+        progressView.setProgress(0, animated: false)
+        reloadData()
         // Do any additional setup after loading the view.
     }
 
+    func reloadData(){
+        // Retrieve current forecast
+       progressView.setProgress(0, animated: true)
+        
+        ForecastIOClient.sharedInstance.forecast(city.latitude, longitude: city.longitude, failure: { (error) in
+            let alert: UIAlertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+            let alertAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+            alert.addAction(alertAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            })
+            { (response, forecastAPICalls) -> Void in
+              self.progressView.setProgress(100, animated: true)
+                
+               print(response.longitude)
+                /*
+                if let numberOfAPICalls: Int = forecastAPICalls {
+                    print("\(numberOfAPICalls) forecastIO API calls made today!")
+                }
+                if let currentTemperature: Double = forecast.currently?.temperature {
+                    self.temperatureLabel.text = String(Int(round(currentTemperature))) + "°"
+                }
+                if let weatherSummary: String = forecast.currently?.summary {
+                    self.weatherSummaryLabel.text = weatherSummary
+                }
+*/
+        }
+
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -41,5 +76,6 @@ class CityDetailViewController: UIViewController {
         print(cities.count)
     
     }
+    
 
 }
